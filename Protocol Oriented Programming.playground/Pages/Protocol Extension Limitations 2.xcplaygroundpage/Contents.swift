7f5c12d66b84790b06d566de3aa7e9cc5d33cc9c
David Waite <david@alkaline-solutions.com>
2015-11-12 17:02:03 -0700

    Changes before meetup

--- Protocol Oriented Programming.playground/Pages/Protocol Extension Limitations 2.xcplaygroundpage/Contents.swift
+++ Protocol Oriented Programming.playground/Pages/Protocol Extension Limitations 2.xcplaygroundpage/Contents.swift
@@ -1,23 +1,32 @@
 /*:
 # Protocol Extension Limitations
 
-Also, you cannot extend a deferred protocol with support for a non-deferred protocol
-*/
+Also, you cannot extend one protocol with support for another
 
-protocol EqualTo {
-    func isEqualTo(other:Any) -> Bool
+*/
+import UIKit
+
+protocol Colorable {
+    func colorize(color:UIColor)
 }
 
 // Error: Extension of protocol 'Equatable' cannot have an inheritance clause
-extension Equatable : EqualTo {
-    func isEqualTo(other:Any) -> Bool {
-        /* ... logic */
-        return false
-    }
+
+protocol Sepiable {
+    func sepiaTone()
 }
 
+/* I can add sepiaTone as a function on all Colorable */
+extension Colorable {
+    func sepiaTone() {
+        colorize(UIColor.brownColor())
+    }
+}
+/* But I can't say all Colorable are Sepiable */
+extension Colorable: Sepiable {} // Error: Extension of protocol cannot have an inheritance clause
+
 /*:
-This is likely because deferred protocols aren't types and don't exist at runtime, only at compile time.
+This is likely because of the dynamic code implications - I would need to track down and alter every type implementing the first protocol, and associate new code to support the second
 
 [⬅ Previous](@previous) || [Next ➡](@next)
 */
